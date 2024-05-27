975e96ca1f84da29cabdd3d454ee41e3e333c8ba
Nishad PT <ptnishad98@gmail.com>
2024-05-27 14:52:59 +0530

    Code lines reduced by adding two numbers in one command

--- main.c
+++ main.c
@@ -3,14 +3,9 @@
 int main() {
   int a, b, sum;
   
-  printf("Enter first integer: ");
-  scanf("%d", &a);
-  printf("Enter second integer: ");
-  scanf("%d", &b);
-  
+  printf("Enter two integers : ");
+  scanf("%d%d", &a, &b);
   sum = a + b;
-  
   printf("The sum of two numbers is %d\n", sum);
-  
   return 0;
 }
